27617e478d123b3602cdaf386f8a526ce9ef9a0e
qteemu <teemu@qissa.fi>
2024-11-06 16:56:09 +0000

    devon..

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-import pandas as pd 
+import pandas as pd
+import numpy as np
 import geopandas as gpd
 from shapely import Point, wkt, ops
 from shapely.wkt import loads, dumps
@@ -13,12 +14,9 @@ import geocoder
 import io
 import requests
 import duckdb
-import ibis
-#ibis: https://ibis-project.org/how-to/configure/basics
-ibis.options.interactive = True
-from ibis import _
-import ibis.selectors as s
-# https://ibis-project.org/posts/ibis-duckdb-geospatial/
+import boto3
+
+import regs
 
 import plotly.express as px
 import plotly.graph_objects as go
@@ -53,61 +51,211 @@ auth = check_password()
 if not auth:
     st.stop()
 
-@st.cache_data()
-def read_allas_file(filepath):
-    allas_url = st.secrets['allas']['allas_url']
-    r = requests.get(f"{allas_url}/{filepath}", stream=True)
-    data = io.BytesIO(r.content)
-    return data
-
 with st.status('Connecting database..') as dbstatus:
-    st.caption(f'Ibis v{ibis.__version__} / Duckdb v{ibis.__version__}')
-    #con = ibis.duckdb.connect(threads=4, memory_limit="4GB") #fine tune depending on instance
-    allas_url = st.secrets['allas']['allas_url']
-    set_s3_endpoint = f"""
-        SET s3_endpoint = '{allas_url}';
-        SET s3_region = 'a3s';
-        SET s3_access_key_id = '{st.secrets['allas']['allas_secret_key']}';
-        SET s3_secret_access_key = '{st.secrets['allas']['allas_access_key']}';
-    """
-    #con.raw_sql(set_s3_endpoint)
-    
-    #st.cache_data(max_entries=1)
-    #def set_view(_s3_path):
-        #con.raw_sql(f"CREATE TEMPORARY TABLE temp_table AS SELECT * FROM parquet_scan('{_s3_path}');")
-        #cfua_table = con.table('temp_table')
-    #    return cfua_table
-    #set_view(f"{allas_url}/RD/{case_file}.parquet")
-    
-    #case
-    case_files = ['Helsinki','Stockholm','Copenhagen','Oslo','Reykjavik']
-    case = st.selectbox('Use data for..',case_files)
-    
-    #ibis way
-    case_file = f"cfua_data_{case}.parquet"
-    sourcepath=f"{allas_url}/CFUA/RD/{case_file}"
-    
+    st.caption(f'Duckdb v{duckdb.__version__}')
+    s3_url = f"{st.secrets['allas']['allas_url']}/CFUA/RD/cfua_data_nordics.parquet"
     @st.cache_data()
-    def read_case(sourcepath):
-        df = ibis.read_parquet(sourcepath).to_pandas()
-        return df
-    
-    df = read_case(sourcepath)
-    st.data_editor(df)
-    
-    #mem ..whole Nordics is too heavy..
-    st.info(f"Memory usage of the data {round(df.memory_usage(deep=True).sum() / (1024**2),3)} MB")
-    
+    def load_db(s3_url):
+        duckdb.sql("INSTALL httpfs;")
+        duckdb.sql("LOAD httpfs;")
+        column_name = 'fua_name'
+        fua_names_query = f"SELECT DISTINCT {column_name} FROM read_parquet('{s3_url}');"
+        fuas = duckdb.query(fua_names_query).to_df()[column_name].tolist()
+        return fuas
+    fuas = load_db(s3_url)
     dbstatus.update(label="DB connected!", state="complete", expanded=False)
 
+s1,s2 =st.columns(2)
+selected_fua = [s1.selectbox("Select case area", fuas)] #take [] away if using multiselect
+df = None
+getit = st.button("Fetch CFUA data")
+if getit:
+    if selected_fua:
+        @st.cache_data()
+        def get_cfua_data(selected_fuas,column='fua_name'):
+            if len(selected_fuas) > 1:
+                # Convert list of selected values into a SQL IN clause format
+                values_str = ', '.join(f"'{v}'" for v in selected_fuas)
+                filter_query = f"""
+                SELECT * 
+                FROM read_parquet('{s3_url}') 
+                WHERE {column} IN ({values_str})
+                """
+            else:
+                value_str = f"'{selected_fuas[0]}'"
+                filter_query = f"""
+                SELECT * 
+                FROM read_parquet('{s3_url}') 
+                WHERE {column} = {value_str}
+                """
+                
+            df = duckdb.query(filter_query).to_df()
+            cols = df.columns.to_list()[:-2]
+            return df[cols]
+        df = get_cfua_data(selected_fuas=selected_fua)
+        df = df.dropna(subset="h3_10").set_index("h3_10")
+        #st.data_editor(df)
+        #st.stop()
+    else:
+        st.warning('Not enough memory..')
+        st.stop()
 
-st.stop()
+if df is None:
+    st.stop()
+    
+# now we need to aggregate and classify the cfua_data
+# to get landuse features of neighborhoods in different sizes for each record
+
+#dict for custom naming the landuse values used as a integers in cfua__data
+lu_dict = {
+        0:"other",
+        1:"dense", #"Discontinuous dense urban fabric (S.L. : 50% -  80%)": 1,
+        2:"compact", #"Discontinuous medium density urban fabric (S.L. : 30% - 50%)": 2,
+        3:"continuous", #"Continuous urban fabric (S.L. : > 80%)": 3,
+        4:"sprawled",#"Discontinuous very low density urban fabric (S.L. : < 10%)": 4,
+        5:"facilities", #"Industrial, commercial, public, military and private units": 5,
+        6:"leisure_sport", #"Sports and leisure facilities": 6,
+        7:"green_area", #"Green urban areas": 7,
+        8:"forest", #"Forests": 8,
+        9:"open_nature"
+        # combine following classes under '9'..
+        #"Open spaces with little or no vegetation (beaches, dunes, bare rocks, glaciers)": 9,
+        #"Herbaceous vegetation associations (natural grassland, moors...)": 9
+        }
+
+#categories for services
+retail_categories = {
+        "Food & Dining": {
+            "restaurant", "cafe", "bar", "pizza_restaurant", "coffee_shop", 
+            "bakery", "italian_restaurant", "thai_restaurant", "sushi_restaurant",
+            "fast_food_restaurant", "wine_bar", "burger_restaurant", 
+            "cocktail_bar", "pub", "food_truck", "ice_cream_shop", "diner",
+            "buffet", "steakhouse", "seafood_restaurant", "vegetarian_restaurant",
+            "breakfast_restaurant", "food_delivery", "food_stand", "cafeteria"
+        },
+        "Shopping & Retail": {
+            "shopping", "clothing_store", "jewelry_store", "grocery_store", 
+            "furniture_store", "flowers_and_gifts_shop", "bookstore", "retail",
+            "shoe_store", "antique_store", "electronics", "sporting_goods",
+            "home_improvement_store", "department_store", "convenience_store",
+            "supermarket", "boutique", "thrift_store", "gift_shop", "market"
+        },
+        "Health & Wellness": {
+            "hospital", "counseling_and_mental_health", "gym", "spas", "dentist",
+            "naturopathic_holistic", "health_and_medical", "yoga_studio",
+            "physical_therapy", "psychotherapist", "psychologist", "fitness_trainer",
+            "acupuncture", "chiropractor", "medical_center", "clinic",
+            "mental_health", "fitness_center", "wellness_center", "pharmacy"
+        },
+        "Professional Services": {
+            "professional_services", "real_estate", "software_development",
+            "advertising_agency", "lawyer", "architectural_designer",
+            "graphic_designer", "financial_service", "marketing_agency",
+            "contractor", "interior_design", "insurance_agency", "accounting",
+            "consulting", "bank", "legal_services", "business_consultant",
+            "web_design", "property_management", "recruitment"
+        },
+        "Arts & Entertainment": {
+            "art_gallery", "theatre", "arts_and_entertainment", "music_venue",
+            "movie_theatre", "museum", "comedy_club", "dance_club",
+            "concert_venue", "performing_arts_venue", "bowling_alley", "casino",
+            "arcade", "amusement_park", "cinema", "nightclub", "art_museum",
+            "cultural_center", "art_school", "entertainment_center"
+        },
+        "Education": {
+            "school", "college_university", "education", "library", "preschool",
+            "language_school", "art_school", "music_school", "high_school",
+            "elementary_school", "tutoring_center", "educational_services",
+            "vocational_school", "dance_school", "driving_school"
+        },
+        "Beauty & Personal Care": {
+            "beauty_salon", "hair_salon", "nail_salon", "barber", "spa",
+            "massage", "tanning_salon", "cosmetics", "beauty_supply",
+            "hair_care", "skin_care", "makeup_artist", "personal_care"
+        },
+        "Community & Religious": {
+            "community_services_non_profits", "religious_organization", 
+            "church_cathedral", "mosque", "temple", "synagogue", 
+            "community_center", "social_services", "charity_organization",
+            "place_of_worship"
+        },
+        "Other": set()  # This will catch any uncategorized amenities
+    }
 
 
-# -------- utils functions -----------
+@st.fragment()
+def aggregation(df):
+    #bin age
+    def binit(df_in,bins = [0, 25, 50, 100]):
+        df = df_in.copy()
+        bins = [0, 25, 50, np.inf]
+        names = ["young", "adult", "senior"]
+        df['Age'] = pd.cut(df['Age'], bins, labels=names)
+        return df
+    df = binit(df)
+    with st.status('Aggregating landuse and services..') as aggstatus:
+        #we first classify service sdi in 'activity_class' column for all hexas in the df
+        df_v2 = regs.classify_service_diversity(df, service_col='services', categories_dict=retail_categories)
+        nd_size_km = st.slider("set radius for the neighborhood to use (km)",1,9,1,step=2)
+        nd_size_dict = {1:7,3:20,5:33,7:47,9:60}
+        nd_size_r = nd_size_dict[nd_size_km]
+        #then we count hexas with different landuse types in the nd of the size set above
+        df_for_reg = regs.count_landuse_types_in_nd(df_in=df_v2, k=nd_size_r, lu_dict=lu_dict)
+        #st.data_editor(df)
+        #st.data_editor(df_v2)
+        #st.data_editor(df_for_reg)
+        #st.stop()
+        del df
+        del df_v2
+        st.data_editor(df_for_reg.describe())
+        st.info(f"Memory usage of the data {round(df_for_reg.memory_usage(deep=True).sum() / (1024**2),1)} MB")
+        aggstatus.update(label="Aggregation ready!", state="complete", expanded=False)
+    return df_for_reg
 
-# H3 -> https://nbviewer.org/github/uber/h3-py-notebooks/blob/master/notebooks/usage.ipynb
-# https://github.com/uber/h3-py-notebooks/blob/master/notebooks/urban_analytics.ipynb
+df_for_reg = aggregation(df)
 
-#dev on..
+with st.expander('Regression',expanded=True):
+    #cols
+    cf_cols = [
+        'Total footprint',
+        'Housing footprint',
+        'Vehicle possession footprint',
+        'Public transportation footprint',
+        'Leisure travel footprint',
+        'Goods and services footprint',
+        'Pets footprint',
+        'Summer house footprint',
+        'Diet footprint',
+        ]
+    lu_cols = [
+            'lu_malls',
+            'lu_continuous',
+            'lu_facilities',
+            'lu_dense',
+            'lu_green_area',
+            'lu_leisure_sport',
+            'lu_compact',
+            'lu_open_nature',
+            'lu_forest']
+    
+    
+    @st.fragment()
+    def generate_regs(df_for_reg):
+        with st.form('reg'):
+            s1,s2,s3 = st.columns(3)
+            target_col=s1.selectbox("Target domain",cf_cols)
+            cat_cols = ['Age','Education level', 'Household per capita income decile', 'Household type']#,'Urban degree']
+            control_cols = s2.multiselect('Control cols',cat_cols,default='Household per capita income decile')
+            base_cols = ['Age','Education level','Household type']
+            ext_cols = lu_cols
+            gen = st.form_submit_button('Generate')
+            
+        if gen:
+            reg_df = regs.ols_reg_table(df_for_reg,target_col,base_cols,cat_cols,ext_cols,control_cols).fillna('-')
+            #st.text(f"cat_cols: {cat_cols}")
+            #st.text(f"base_cols: {base_cols}")
+            #st.text(f"ext_cols: {ext_cols}")
+            st.data_editor(reg_df,use_container_width=True, height=700)
 
+    generate_regs(df_for_reg=df_for_reg)
+    
---
+++ app/regs.py
@@ -0,0 +1,203 @@
+import pandas as pd
+import numpy as np
+import h3
+import h3.api.basic_int as h3_api
+import ast
+
+#ols regression
+import pingouin as pg
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+
+def ols_reg_table(df_in, cf_col, base_cols, cat_cols, ext_cols, control_cols):
+    df = df_in.copy()
+    
+    def ols(df, cf_col, base_cols, cat_cols, ext_cols, control_cols):
+        cat_cols_lower = [col.lower().replace(' ', '_') for col in cat_cols]
+        
+        def format_col(col):
+            col_lower = col.lower().replace(' ', '_')
+            return f'C({col_lower})' if col_lower in cat_cols_lower else col_lower
+            
+        domain_col = cf_col.lower().replace(' ', '_')
+        df.columns = df.columns.str.lower().str.replace(' ', '_')
+        
+        base_cols_str = ' + '.join([format_col(col) for col in base_cols])
+        ext_cols_str = ' + '.join([col.lower().replace(' ', '_') for col in ext_cols])
+        if control_cols is not None:
+            control_cols_str = ' + '.join([format_col(col) for col in control_cols])
+            base_formula = f'{domain_col} ~ {base_cols_str} + {control_cols_str}'
+            ext_formula = f'{domain_col} ~ {base_cols_str} + {ext_cols_str} + {control_cols_str}'
+        else:
+            base_formula = f'{domain_col} ~ {base_cols_str}'
+            ext_formula = f'{domain_col} ~ {base_cols_str} + {ext_cols_str}'
+        
+        base_model = smf.ols(formula=base_formula, data=df)
+        base_results = base_model.fit()
+        ext_model = smf.ols(formula=ext_formula, data=df)
+        ext_results = ext_model.fit()
+        
+        return base_results, ext_results
+    
+    base_results, ext_results = ols(df, cf_col, base_cols, cat_cols, ext_cols, control_cols)
+    rounder = 3
+    reg_results = pd.DataFrame({
+        'base_r': round(base_results.params, rounder),
+        'base_p': round(base_results.pvalues, rounder),
+        'ext_r': round(ext_results.params, rounder),
+        'ext_p': round(ext_results.pvalues, rounder)
+    })
+    return reg_results
+
+def partial_corr(df,cf_cols,corr_target,covar='Income level decile'):
+    df_all = pd.DataFrame()
+    for c in cf_cols:
+        row = pg.partial_corr(data=df, x=c, y=corr_target, x_covar=covar, method='pearson')
+        domain_index_name = f"{corr_target} vs {c}"
+        row['target_domains'] = domain_index_name
+        for col in ['r','p-val']:
+            row[col] = round(row[col],3)
+        df_all = pd.concat([df_all,row])
+    df_all.set_index('target_domains', inplace=True)
+    return df_all
+
+
+# ------- services aggregation and sdi classification --------
+
+def classify_service_diversity(df, service_col='services', categories_dict=None):
+    def parse_and_categorize(service_str):
+        # Parse service string to dictionary
+        if pd.isna(service_str) or service_str == 'nan':
+            return {}
+        try:
+            service_dict = ast.literal_eval(service_str)
+        except:
+            return {}
+        
+        # Categorize services using categories_dict
+        categorized_counts = {category: 0 for category in categories_dict.keys()}
+        for service, count in service_dict.items():
+            service = service.lower().replace(' ', '_')
+            for category, items in categories_dict.items():
+                if service in items:
+                    categorized_counts[category] += count
+                    break
+        
+        return categorized_counts
+    
+    def calculate_sdi(categorized_counts):
+        # Remove 'Other' category and get values
+        if 'Other' in categorized_counts:
+            del categorized_counts['Other']
+        
+        values = np.array(list(categorized_counts.values()))
+        total = values.sum()
+        
+        # If no services or only one type, return 0
+        if total <= 0:
+            return 0
+        
+        # Calculate proportions and remove zeros
+        proportions = values[values > 0] / total
+        
+        # Calculate Shannon index
+        return round(-np.sum(proportions * np.log(proportions)), 1)
+    
+    # Create a copy of the DataFrame
+    df_copy = df.copy()
+    
+    # Calculate SDI for each row
+    sdis = []
+    for service_str in df_copy[service_col]:
+        categorized = parse_and_categorize(service_str)
+        sdi = calculate_sdi(categorized)
+        sdis.append(sdi)
+    
+    df_copy['service_sdi'] = sdis
+    
+    def assign_activity_class(series):
+        # Calculate quartiles for non-zero SDI values
+        non_zero_sdis = series[series > 0]
+        if len(non_zero_sdis) > 0:
+            q1, q2, q3, q4 = np.percentile(non_zero_sdis, [25, 50, 75, 90])
+            
+            # Create the classification function
+            def classify(x):
+                if x == 0:
+                    return 0
+                elif x <= q1:
+                    return 0
+                elif x <= q2:
+                    return 1
+                elif x <= q3:
+                    return 2
+                elif x <= q4:
+                    return 3
+                else:
+                    return 4
+        else:
+            # If no non-zero SDI values, all get class 0
+            def classify(x):
+                return 0
+        
+        # Apply the classification function to the series
+        return series.apply(classify)
+    
+    # Apply the activity classification
+    df_copy['activity_class'] = assign_activity_class(df_copy['service_sdi']).fillna(0)
+    
+    return df_copy
+
+
+def count_landuse_types_in_nd(df_in, k, lu_dict):
+    df = df_in.copy()
+    # extract cf records (those hexes which have footprint values)
+    cf_points = df[df['Total footprint'] != 0]
+    hex_list = cf_points.index.tolist()
+    
+    # Convert hex ids from strings to H3 integers and generate the k-ring neighborhoods
+    #neighborhoods_as_h3ids = {hex_str: h3_api.k_ring(h3_api.string_to_h3(hex_str), k) for hex_str in hex_list}
+    neighborhoods_as_h3ids = {hex_str: h3.grid_disk(hex_str, k) for hex_str in hex_list}
+
+    # Loop through each hexagon's neighborhood
+    for hex_str, hex_ring in neighborhoods_as_h3ids.items():
+        # Filter the dataframe for hexagons in the neighborhood
+        neighborhood_df = df[df.index.isin(hex_ring)]
+        if neighborhood_df.empty:
+            continue
+        
+        # Replace landuse names from dict
+        nd_df = neighborhood_df.copy().replace({'landuse_class': lu_dict})
+
+        # Count landuse_class counts in nd and update df accordingly in center hex
+        landuse_counts = nd_df['landuse_class'].value_counts()
+        for landuse_class, count in landuse_counts.items():
+            if f"lu_{landuse_class}" in df.columns:
+                df.at[hex_str, f'lu_{landuse_class}'] += count
+            else:
+                df[f"lu_{landuse_class}"] = 0
+                df.at[hex_str, f'lu_{landuse_class}'] = count
+        
+        #count "malls" (activity class 3 or 4 = high sdi) accordingly..
+        if 'activity_class' in nd_df.columns:
+            mall_count = nd_df[nd_df['activity_class'] > 2].shape[0]
+            df[f"lu_malls"] = 0
+            df.at[hex_str, 'lu_malls'] = int(mall_count)
+            
+    lu_columns = [col for col in df.columns if col.startswith('lu_')]
+    for col in lu_columns:
+        if col not in df.columns:
+            df[col] = 0
+    
+    #normalize values
+    def normalize_df(df,cols=None):
+        if cols is None:
+            cols = df.columns.tolist()
+        for col in cols:
+            df[col] = round(df[col] / df[col].abs().max(),1)
+        return df
+    
+    out_df = normalize_df(df,cols=lu_columns)
+    
+    return out_df 
+
